f23b034ecfc29579cd6f3a6978a383e4674ef8d9
emiliwillinghoefer <emili.w@unochapeco.edu.br>
2023-08-16 22:36:26 -0300

    init

---
+++ app/index.php
@@ -0,0 +1,18 @@
+<?php
+
+class Servico
+{
+    protected string $servico;
+
+    /**
+     * @param string $servico
+     */
+    public function __construct(string $servico)
+    {
+        $this->servico = $servico;
+    }
+
+
+
+
+}
